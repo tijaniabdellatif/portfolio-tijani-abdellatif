b0a620ebbe9828107f627232f57ad7fd65722efe
abdellatif tijani <tijani.idrissi.abdellatif@gmail.com>
2025-04-24 22:17:47 +0100

    setup projecty

---
+++ components/Hero.tsx
@@ -0,0 +1,32 @@
+import React from "react";
+import clsx from "clsx";
+import { Spotlight } from "./ui/SpotLight";
+
+const Hero = () => {
+
+    return (
+        <div className={clsx([
+            'pb-20 pt-36'
+        ])}>
+            <div>
+                <Spotlight fill="#D4AF37" className={clsx([
+                    "-top-40 -left-40",
+                    "md:-left-32 md:-top-20",
+                    'h-screen'
+                ])} />
+
+                <Spotlight fill="#6D28D9" className={clsx([
+                    "top-10 left-full",
+                    'h-[80vh] w-[50vw]',
+                ])} />
+
+                <Spotlight fill="#4169E1" className={clsx([
+                    "top-28 left-80",
+                    'h-[80vh] w-[50vw]',
+                ])} />
+            </div>
+        </div>
+    );
+}
+
+export default Hero;
