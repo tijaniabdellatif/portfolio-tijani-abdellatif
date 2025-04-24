07e80fe758a0aaf028e7e2ce28e5ee1fc961f4fd
abdellatif tijani <tijani.idrissi.abdellatif@gmail.com>
2025-04-24 22:40:13 +0100

    re organize implementation

--- index.d.ts
+++ index.d.ts
@@ -1,4 +1,25 @@
+declare module "tailwindcss/lib/util/color" {
+  const parseColor: (value: string) => { color: Array<string> };
+  export { parseColor };
+}
+
+// Define or import the Config type
+// If Config is defined in your code, you need to declare it, e.g.,
+// type Config = {/* define the structure here */};
+
+// If Config is from an external library, import it like this:
+// import { Config } from 'some-library';
+
 declare module "tailwindcss" {
-    import { Config } from "tailwindcss/types/config";
-    export type { Config };
-  }
+  import { Config } from "tailwindcss/types/config";
+  export type { Config };
+}
+declare module "tailwindcss/lib/util/flattenColorPalette" {
+  const flattenColorPalette: (colors: any) => Record<string, string>;
+  export default flattenColorPalette;
+}
+
+declare module "tailwindcss/colors" {
+  const colors: { [key: string]: any };
+  export default colors;
+}
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -26,15 +26,6 @@ const config = {
       },
     },
     extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
-      },
-      fontFamily: {
-        heading: ["var(--font-heading)", "sans-serif"],
-        body: ["var(--font-body)", "sans-serif"],
-      },
       colors: {
         black: {
           DEFAULT: "#000",
@@ -90,7 +81,6 @@ const config = {
         md: "calc(var(--radius) - 2px)",
         sm: "calc(var(--radius) - 4px)",
       },
-
       keyframes: {
         "accordion-down": {
           from: { height: "0" },
