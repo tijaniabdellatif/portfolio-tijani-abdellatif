9b9041f4039c0ed4c8ed58a4fa2af5c3882aebac
abdellatif tijani <tijani.idrissi.abdellatif@gmail.com>
2025-04-26 15:19:33 +0100

    redeploy

--- utils/animation.ts
+++ utils/animation.ts
@@ -5,7 +5,7 @@ export const fadeIn: Variants = {
     visible: {
       opacity: 1,
       y: 0,
-      transition: { duration: 0.6, ease: "easeOut" }
+      transition: { duration: 0.7, ease: "easeOut" }
     }
   };
   
